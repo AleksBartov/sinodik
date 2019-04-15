6088fe98e95b5bc3660024e0b871f495d249506c
Aleksandr <aleksandrbartov@yandex.ru>
2019-04-16 02:29:48 +0400

    hidden menus

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,13 @@
+//alert('i am here');
 
 
-  console.log(document.querySelectorAll('.logoContainer path')[5].getTotalLength());
+// --gettingCorrentPathLength---
+
+console.log(document.querySelectorAll('.logoContainer path')[5].getTotalLength());
 
 
 
+// ---hiddenMenuOpenning-------
 
 
 const sandwich = document.querySelector('.sandwich'),
@@ -15,26 +19,45 @@ hM = document.querySelector('.hiddenMenu'),
 hiddenMenuLinks = document.querySelectorAll('.hiddenMenu-link');
 
 
+let menuOpened = false;
+
 
 sandwich.addEventListener('click', ()=>{
 
+
+menuOpened = !menuOpened;
+
 hM.classList.toggle('openedMenu');
 
-  Array.from(sandwichItems).forEach((i, index)=>{
 
-  i.classList.toggle(`opened_${index}`);
+Array.from(sandwichItems).forEach((i, index)=>{
+
+i.classList.toggle(`opened_${index}`);
 
 })
 
 }, true);
 
 
+hM.addEventListener('touchmove', (e)=>{
+
+e.preventDefault();
+
+}, false);
+
+
 
 hM.addEventListener('transitionend', ()=>{
 
-  
+if (menuOpened){
 
-hiddenMenuLinks[0].classList.toggle('linkToView');
+hiddenMenuLinks[0].classList.add('linkToView');
+
+}else{
+
+hiddenMenuLinks[0].classList.remove('linkToView');
+
+}
 
 }, true);
 
@@ -42,36 +65,224 @@ hiddenMenuLinks[0].classList.toggle('linkToView');
 
 hiddenMenuLinks[0].addEventListener('transitionend', ()=>{
 
-  
+
+
+if (menuOpened){
 
 hiddenMenuLinks[1].classList.add('linkToView');
 
+}else{
 
-}, true);
+hiddenMenuLinks[1].classList.remove('linkToView');
 
-
-hiddenMenuLinks[1].addEventListener('transitionend', ()=>{
-
-  
-
-hiddenMenuLinks[2].classList.add('linkToView');
-
+}
 
 }, true);
 
 
 
-hiddenMenuLinks[2].addEventListener('transitionend', ()=>{
+hiddenMenuLinks[0].addEventListener('click', ()=>{
 
-  
 
-hiddenMenuLinks[3].classList.add('linkToView');
+menuOpened = !menuOpened;
+
+hM.classList.toggle('openedMenu');
+
+
+Array.from(sandwichItems).forEach((i, index)=>{
+
+i.classList.toggle(`opened_${index}`);
+
+})
+
+
+formOpened = true;
+
+hiddenRoom.classList.add('openedRoom');
 
 
 }, true);
 
 
 
+
+
+// ---endHiddenMenuOpenning----
+
+
+
+
+
+
+
+
+
+// ----personRoomOpenning------
+
+
+
+const room = document.querySelector('.personRoom'),
+
+hiddenRoom = document.querySelector('.hiddenRoom'),
+
+arrowRegisterBack = document.querySelector('.registerHeaderTitle svg'),
+
+linkToRegModal = document.querySelector('.linkToRegModal'),
+
+hiddenFormItems = document.querySelectorAll('.hiddenFormItem');
+
+
+
+let formOpened = false;
+
+
+room.addEventListener('click', ()=>{
+
+
+formOpened = true;
+
+hiddenRoom.classList.add('openedRoom');
+
+}, true);
+
+
+
+hiddenRoom.addEventListener('touchmove', (e)=>{
+
+e.preventDefault();
+
+}, false);
+
+
+
+linkToRegModal.addEventListener('click', ()=>{
+
+
+formOpened = true;
+
+hiddenRoom.classList.add('openedRoom');
+
+}, true);
+
+
+
+hiddenRoom.addEventListener('transitionend', ()=>{
+
+if (formOpened){
+
+hiddenFormItems[0].classList.add('linkToView');
+
+}else{
+
+hiddenFormItems[0].classList.remove('linkToView');
+
+}
+
+}, true);
+
+
+hiddenFormItems[0].addEventListener('transitionend', ()=>{
+
+if (formOpened){
+
+hiddenFormItems[1].classList.add('linkToView');
+
+}else{
+
+hiddenFormItems[1].classList.remove('linkToView');
+
+}
+
+}, true);
+
+
+hiddenFormItems[1].addEventListener('transitionend', ()=>{
+
+if (formOpened){
+
+hiddenFormItems[2].classList.add('linkToView');
+
+}else{
+
+hiddenFormItems[2].classList.remove('linkToView');
+
+}
+
+}, true);
+
+
+hiddenFormItems[2].addEventListener('transitionend', ()=>{
+
+if (formOpened){
+
+hiddenFormItems[3].classList.add('linkToView');
+
+}else{
+
+hiddenFormItems[3].classList.remove('linkToView');
+
+}
+
+}, true);
+
+
+
+hiddenFormItems[3].addEventListener('transitionend', ()=>{
+
+if (formOpened){
+
+hiddenFormItems[4].classList.add('linkToView');
+
+}else{
+
+hiddenFormItems[4].classList.remove('linkToView');
+
+}
+
+}, true);
+
+
+
+hiddenFormItems[4].addEventListener('transitionend', ()=>{
+
+if (formOpened){
+
+hiddenFormItems[5].classList.add('linkToView');
+
+}else{
+
+hiddenFormItems[5].classList.remove('linkToView');
+
+}
+
+}, true);
+
+
+
+arrowRegisterBack.addEventListener('click', ()=>{
+
+formOpened = false;
+
+hiddenRoom.classList.remove('openedRoom');
+
+}, true);
+
+
+
+
+
+// ----endPersonRoomOpenning---
+
+
+
+
+
+
+
+
+
+
+// ---paragfafAppierCode-------
 
 
 
@@ -84,19 +295,19 @@ function checkSlide(e) {
 Array.from(paragrafs).forEach(p=>{
 
 
-    const slideInAt = (window.scrollY + window.innerHeight) - p.scrollHeight/2;
+  const slideInAt = (window.scrollY + window.innerHeight) - p.scrollHeight/2;
 
-    const pBottom = p.offsetTop + p.scrollHeight;
+  const pBottom = p.offsetTop + p.scrollHeight;
 
-    const isHalfShown = slideInAt > p.offsetTop;
+  const isHalfShown = slideInAt > p.offsetTop;
 
-    const isNotScrolledPast = window.scrollY < pBottom;
-
-
-    
+  const isNotScrolledPast = window.scrollY < pBottom;
 
 
-    if(isHalfShown && isNotScrolledPast) p.classList.add('active')
+//console.log(p.height);
+
+
+  if(isHalfShown && isNotScrolledPast) p.classList.add('active')
 
 });
 
@@ -108,29 +319,29 @@ window.addEventListener('scroll', debounce(checkSlide), true);
 
 function debounce(func, wait = 20, immediate = true) {
 
-  var timeout;
+var timeout;
 
-  return function() {
+return function() {
 
-    var context = this, args = arguments;
+  var context = this, args = arguments;
 
-    var later = function() {
+  var later = function() {
 
-      timeout = null;
+    timeout = null;
 
-      if(!immediate) func.apply(context, args);
-
-    };
-
-    var callNow = immediate && !timeout;
-
-    clearTimeout(timeout);
-
-    timeout = setTimeout(later, wait);
-
-    if(callNow) func.apply(context, args);
+    if(!immediate) func.apply(context, args);
 
   };
+
+  var callNow = immediate && !timeout;
+
+  clearTimeout(timeout);
+
+  timeout = setTimeout(later, wait);
+
+  if(callNow) func.apply(context, args);
+
+};
 
 };
 
@@ -138,8 +349,4 @@ function debounce(func, wait = 20, immediate = true) {
 
 
 
-function toHideAll() {
-
-  document.body.style.opacity = 0;
-
-}
+// ---EndParagfafAppierCode-----
